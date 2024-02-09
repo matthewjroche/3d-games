54e7b8a4a7ac7d9ae2d9c8248a5e9646dfbc4afc
matt <r014882k@student.staffs.ac.uk>
2024-02-09 18:10:48 +0000

    added 19x19 gobban, removed zoom

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "https://threejs.org/build/three.module.js";
 import { GUI } from "dat.gui";
-// import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
+
 //SCENE
 const scene = new THREE.Scene();
 
@@ -30,52 +30,67 @@ gui
   .listen();
 
 //CAMERA
-
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  10000 
 );
+camera.position.y = 5;
+camera.position.z = 5;
+camera.lookAt(0, 0, 0);
+// camera.position.set( 0, 20, 100 );
+// controls.update();
 
-camera.position.z = 2;
 
+//RENDERER
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+
 
 //WINDOW RESIZE
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // render()
 }
 
-
-//CUBE
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
+//RAYCASTING
 
 // Render the scene
 function animate() {
-    controls.update();
 
   requestAnimationFrame(animate);
+  controls.update();
 
   // Rotate the cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
 
   // Render the scene
   renderer.render(scene, camera);
 }
 
 animate();
+
+//LOAD TEXTURES
+const texture1 = new THREE.TextureLoader().load('/textures/19x19.png' );
+
+
+
+
+//CUBE
+const geometry = new THREE.BoxGeometry(4.5, 0.1, 4.5);
+// const material = new THREE.MeshBasicMaterial({ color: "#e09d38" });
+const material = new THREE.MeshBasicMaterial( { map:texture1 } );
+
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
